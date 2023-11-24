b745a59be479aa4ae4a801e7be959391bae8e027
vicwin13 <vnwimo13@gmail.com>
2023-11-24 23:56:36 +0100

    almost there

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+
+// This is for the notification and the profile dropdowns
+
 const bell = document.getElementById('bell');
 const bellDropDown = document.getElementById('bell-drop-down');
 const profile = document.getElementById("profile");
 const profileDropDown = document.getElementById("profile-drop-down")
 
+
+// for the notification bell
 
 bell.addEventListener('click', function(event){
 
@@ -23,6 +28,8 @@ document.addEventListener('click', function(event) {
     }
 });
 
+// for the profile dropdown
+
 profile.addEventListener('click', function (event){
     const expanded = this.getAttribute('aria-expanded') === 'true' || false;
     this.setAttribute('aria-expanded', !expanded)
@@ -42,5 +49,83 @@ document.addEventListener('click', function(event){
        profileDropDown.style.display = 'none';
     }
 })
+
+// This code is to use the cancel button to take out a container
+
+const start = document.querySelector('.start');
+const cancel = document.querySelector('.cancel');
+const arrow = document.querySelector('.down-arrow')
+
+cancel.addEventListener('click', function(){
+   start.style.display = 'none'
+   
+})
+
+
+// This is for the display of the setup-guide
+
+const firstSetup = document.querySelector('.first-setup');
+const sectTwo = document.querySelectorAll(['.section-two']);
+
+firstSetup.addEventListener('click', function(){
+    sectTwo.forEach((item)=>{
+        setTimeout(()=>{
+        item.classList.toggle('section-two-show');
+            
+        }, 200)
+        arrow.classList.toggle('up-arrow')
+    })
+})
+
+
+// const increase = document.querySelector('.increament');
+
+// This is for the dropdowns inside the setup guide
+
+const allSections = document.querySelectorAll('.section-two')
+const customize = document.querySelectorAll('.customize');
+const hidden = document.querySelectorAll('.hide')
+customize.forEach((customItem, index)=>{
+   customItem.addEventListener('click', function(){
+
+    allSections.forEach((sections, i)=>{
+        if( i === index){
+            
+                sections.classList.remove('show-hidden')
+                console.log(sections)
+            
+        }
+    })
+
+   const currentSelection = customItem.closest('.section-two')
+   currentSelection.classList.toggle('show-hidden')
+
+   hidden.forEach((item)=>{
+    if(!currentSelection.contains(item)){
+        item.classList.toggle('show-hidden')
+    }
+   })
+
+})
+
+})
+
+
+
+
+// for the progress bar
+
+function updateProgressBar() {
+    
+    const progressBar = document.getElementById('load');
+  
+    progressBar.value = customize.length
+
+    console.log(progressBar)
+  }
+
+  
+
+
 
 
